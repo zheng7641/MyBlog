84e7c31339a893ee95e136f78c3000d022d49566
zheng7641 <zheng7641@163.com>
2018-04-02 23:37:56 +0800

    run

--- src/main/java/com/zheng/controller/TestController.java
+++ src/main/java/com/zheng/controller/TestController.java
@@ -2,12 +2,14 @@ package com.zheng.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("test")
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping("test1")
+    @ResponseBody
     public String Test() {
         return "aaa";
     }
