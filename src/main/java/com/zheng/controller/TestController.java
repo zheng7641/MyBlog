107dca3767c669bae5232b05fd615cae671b4ee6
zheng7641 <zheng7641@163.com>
2018-04-04 00:40:07 +0800

    run

--- src/main/java/com/zheng/controller/TestController.java
+++ src/main/java/com/zheng/controller/TestController.java
@@ -1,16 +1,22 @@
 package com.zheng.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("test2")
 public class TestController {
 
     @RequestMapping("test1")
-    public String Test() {
+    public String Test(ModelMap model) {
         System.out.println("test111");
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("blog","blog1");
+        model.addAllAttributes(result);
         return "index.html";
 
     }
